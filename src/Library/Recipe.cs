a20a4ef497309fcf51bbc840cdad3ce96e51aa42
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-04-20 16:24:46 -0300

    GetProductionCost y responsabilidad

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -25,6 +25,41 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        /// <summary>
+        /// En este caso se utiliza expert para la asignación de responsabilidades; como la clase recipe
+        /// tiene todos los datos necesarios para crear el método, se crea el mismo dentro de esa clase.
+        /// De haber querido utilizar SRP, se tendría que separar PrintRecipe de la clase recipe,
+        /// debido a que como ya tiene metodos para añadir y quitar elementos del ArrayList steps,
+        /// ya tendría dos razones de cambio, una sería si por ejemplo se quiere cambiar el texto a imprimir,
+        /// y otra sería si se quisiera cambiar la implementación de steps, por ejemplo cambiando la forma
+        /// en la que se almacenan los datos.
+        /// También se tendría que crear una nueva clase para el cálculo del costo de producción
+        /// y enviarle todos los datos necesarios a la misma, lo que incluiría modificar el atributo steps
+        /// para poder acceder a los datos desde fuera de recipe, y luego poder realizar
+        /// la implementación del método pedido y enviar el costo de producción a la impresión de la receta.
+        /// </summary>
+        /// <returns></returns>
+
+        public double GetProductionCost()
+        {
+            double sumaInsumos = 0;
+            double costoEquipamiento;
+            double sumaTiempoCosto = 0;
+            double sumaTiempoPasos = 0;
+            double total = 0;
+            foreach (Step step in this.steps)
+            {
+                // el costo de los insumos es el precio del insumo por la cantidad necesaria
+                sumaInsumos = sumaInsumos + (step.Quantity * step.Input.UnitCost);
+                // para calcular el costo de equipamiento se necesitan el tiempo de uso, el costo, y la hora del equipo para todos los pasos de la receta. En este caso, solamente es posible conseguir un dato de tiempo, que se encuentra en step.time. Por ende, la cuenta a realizar sería step.time*(costo del equipamiento)/(la suma de todos los step.time del ArrayList)
+                sumaTiempoCosto = sumaTiempoCosto + (step.Equipment.HourlyCost * step.Time);
+                sumaTiempoPasos = sumaTiempoPasos + step.Time;
+            }
+            costoEquipamiento = sumaTiempoCosto / sumaTiempoPasos;
+            total = sumaInsumos + costoEquipamiento;
+            return total;
+        }
+
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
@@ -33,6 +68,7 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+            Console.WriteLine($"costo total de producción: {GetProductionCost()}");
         }
     }
 }
